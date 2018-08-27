3e875f4d63eee8b240cfc93fd76812f4b8d17480
刘卫星 <liuweixing177@hfjy.com>
2018-08-27 19:43:07 +0800

    用户操作

--- apps/user_operation/adminx.py
+++ apps/user_operation/adminx.py
@@ -1,2 +1,21 @@
 # __author__ = 'wilsonLwx'
 # __date__ = '2018-8-27'
+import xadmin
+from user_operation.models import UserFav, UserLeavingMessage, UserAddress
+
+
+class UserFavAdmin(object):
+    list_display = ["user", "goods", "add_time"]
+
+
+class UserLeavingMessageAdmin(object):
+    list_display = ["user", "message_type", "message", "add_time"]
+
+
+class UserAddressAdmin(object):
+    list_display = ["signer_name", "signer_mobile", "district", "address"]
+
+
+xadmin.site.register(UserFav, UserFavAdmin)
+xadmin.site.register(UserLeavingMessage, UserLeavingMessageAdmin)
+xadmin.site.register(UserAddress, UserAddressAdmin)
