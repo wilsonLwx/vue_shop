0e24ff54e8047d77f2622f7c08c1fde8bf81028f
刘卫星 <liuweixing177@hfjy.com>
2018-08-27 19:35:47 +0800

    订单交易

--- apps/trade/adminx.py
+++ apps/trade/adminx.py
@@ -1,2 +1,24 @@
 # __author__ = 'wilsonLwx'
 # __date__ = '2018-8-27'
+import xadmin
+from trade.models import ShoppingCart, OrderGoods, OrderInfo
+
+
+class ShoppingCartAdmin(object):
+    list_display = ["user", "goods", "nums"]
+
+
+class OrderInfoAdmin(object):
+    list_display = ["user", "order_sn", "trade_no", "pay_status", "post_script", "order_mount", "pay_time", "add_time"]
+
+    class OrderGoodsInline(object):
+        model = OrderGoods
+        exclude = ["add_time"]
+        extra = 1
+        style = "tab"
+
+    inline = [OrderGoodsInline]
+
+
+xadmin.site.register(ShoppingCart, ShoppingCartAdmin)
+xadmin.site.register(OrderInfo, OrderInfoAdmin)
